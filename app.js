6cbe398162007df8e8c692a1640e8737ab0169e8
August Connolly <aconnolly2@mail.csuchico.edu>
2022-09-15 15:10:02 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express'); //Import the express dependency
+const app = express();              //Instantiate an express app, the main work horse of this server
+const port = 5000;                  //Save the port number where your server will be listening
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '127.0.0.1';
+// Listen on a specific port via the PORT environment variable
+var proxyport = process.env.PORT || 8080;
+
+
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(proxyport, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + proxyport);
+});
+
+//Idiomatic expression in express to route and respond to a client request
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('marker.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+});
+
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port ${port}`); 
+});
